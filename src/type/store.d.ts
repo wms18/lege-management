af47ce20f18acce6b0afac548f633f3d89a4d87c
wms18 <1532601761@qq.com>
2022-11-05 15:04:14 +0800

    1

--- src/type/store.d.ts
+++ src/type/store.d.ts
@@ -4,6 +4,6 @@
  * @Author: 吴毛三
  * @Date: 2022-11-05 14:48:33
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-11-05 14:56:29
+ * @LastEditTime: 2022-11-05 15:03:31
  */
 type RootState = ReturnType<typeof import("@/store").getState>;
