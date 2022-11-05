f78c2b02869002f0beea440521468a1c0e173153
wms18 <1532601761@qq.com>
2022-11-05 14:57:33 +0800

    type

---
+++ src/type/store.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-11-05 14:48:33
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-11-05 14:56:29
+ */
+type RootState = ReturnType<typeof import("@/store").getState>;
